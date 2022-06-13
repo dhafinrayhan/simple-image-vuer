fc754236c6b83b4489685ae4daaa3f9f845e2ab9
Dhafin Rayhan <dhafindiamle@gmail.com>
2022-06-13 23:21:07 +0700

    add image thumbnail hover & selected style

--- src/App.vue
+++ src/App.vue
@@ -18,24 +18,25 @@ export default {
       this.images = await (await fetch(API_URL)).json();
     },
     increaseIndex() {
-      if (this.selectedIndex<this.images.length-1) this.selectedIndex++;
+      if (this.selectedIndex < this.images.length - 1) this.selectedIndex++;
     },
     decreaseIndex() {
-      if (this.selectedIndex>0) this.selectedIndex--;
+      if (this.selectedIndex > 0) this.selectedIndex--;
     }
   }
 }
 </script>
 
 <template>
-  <button v-if="this.selectedIndex!==null" @click="this.selectedIndex = null">X</button>
-  <img v-if="this.selectedIndex!==null" :src="this.images[selectedIndex].url" class="image-view">
-  <p v-if="this.selectedIndex!==null">{{ this.images[this.selectedIndex].url }}</p>
-  <button v-if="this.selectedIndex!==null" @click="decreaseIndex">left</button>
-  <button v-if="this.selectedIndex!==null" @click="increaseIndex">right</button>
+  <button v-if="this.selectedIndex !== null" @click="this.selectedIndex = null">X</button>
+  <img v-if="this.selectedIndex !== null" :src="this.images[selectedIndex].url" class="image-view">
+  <p v-if="this.selectedIndex !== null">{{ this.images[this.selectedIndex].url }}</p>
+  <button v-if="this.selectedIndex !== null" @click="decreaseIndex">left</button>
+  <button v-if="this.selectedIndex !== null" @click="increaseIndex">right</button>
   <ul>
     <li v-for="(image, index) in images">
-      <img :src="image.url" @click="this.selectedIndex = index" class="image-preview">
+      <img :src="image.url" @click="this.selectedIndex = index" class="image-thumbnail"
+        :class="{ 'image-thumbnail-selected': this.selectedIndex === index }">
     </li>
   </ul>
 </template>
@@ -48,10 +49,18 @@ export default {
   margin-right: auto;
 }
 
-.image-preview {
+.image-thumbnail {
   width: 144px;
   height: 80px;
   object-fit: cover;
+}
+
+.image-thumbnail-selected,
+.image-thumbnail:hover {
+  width: 160px;
+  height: 96px;
+  object-fit: cover;
+  border: 4px solid blue;
 }
 
 ul {
